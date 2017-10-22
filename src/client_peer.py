4fef65ce1ba2375a50a2b6264c8377a0fd22c36f
Chinmay Rudrapatna <csrudrap@ncsu.edu>
2017-10-22 10:19:28 -0400

    Creating an incomplete client_peer.py

---
+++ src/client_peer.py
@@ -0,0 +1,17 @@
+# client_peer.py
+# Implement rfc_query and get_rfc functions.
+# Store a list of RFC index values.
+# Each client will have its own index list, class variable.
+# 
+
+
+
+class Peer_Client:
+    #  
+
+class Index:
+    i.rfc_num, i.rfc_title, i.peer_hostname
+    def __init__(self, rfc_num, rfc_title, peer_hostname):
+        self.rfc_num = rfc_num
+        self.rfc_title = rfc_title
+        self.peer_hostname = peer_hostname
