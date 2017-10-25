be55c4d581b6b07ab395cedfbe7128d593334905
Chinmay Rudrapatna <csrudrap@ncsu.edu>
2017-10-25 04:32:46 +0000

    After p2p with 2 peers worked.

--- src/peerA.py
+++ src/peerA.py
@@ -3,6 +3,7 @@
 import server_peer as sp
 import client
 import socket
+import time
 
 rs_hostname = '10.0.0.1'
 rs_port = 65423
@@ -10,11 +11,11 @@ rfc_server_port = 60001
 
 c = client.Client(rs_hostname,rs_port,rfc_server_port)
 c.register()
+time.sleep(4)
 c.pquery()
 flag = False;
 rfcsearch = "8266"
 for i in c.active_peers:
-    print "HNAME: {}, SOCKET-HOSTNAME: {}, equal?".format(i.hostname, socket.gethostname(), i.hostname != socket.gethostname)
     if i.hostname != socket.gethostname():
         print i.hostname,socket.gethostname()
         c.rfcquery(i.hostname,i.rfc_server_port)
@@ -22,9 +23,8 @@ for i in c.active_peers:
         indexlist = sp.Server_Peer.get_indexlist()
         print indexlist
         for j in indexlist:
-            print "THIS HAPPENED", j.rfc_num
             if j.rfc_num == rfcsearch:
-                print "Peer B has required RFC!!"
+                print "{} has required RFC {}.".format(j.peer_hostname, rfcsearch)
                 flag = True
                 hostname = j.peer_hostname
 if flag:
--- src/peerB.py
+++ src/peerB.py
@@ -2,25 +2,29 @@
 
 import server_peer as sp
 import client
+import socket
+import time
 
 rs_hostname = '10.0.0.1'
 rs_port = 65423
 rfc_server_port = 60002
-
 c = client.Client(rs_hostname,rs_port,rfc_server_port)
 c.register()
-'''
+time.sleep(4)
 c.pquery()
 flag = False;
-rfcsearch = "8266"
+rfcsearch = "8029"
 for i in c.active_peers:
-    c.rfcquery(i.hostname,i.rfc_server_port)
-    for j in Client.indexlist:
-        if j.rfc_num == rfcsearch:
-            print "Peer B has required RFC!!"
-            flag = True
-            hostname = j.peer_hostname
-
+    if i.hostname != socket.gethostname():
+        print i.hostname,socket.gethostname()
+        c.rfcquery(i.hostname,i.rfc_server_port)
+        print "Completed RFC Query in peer B"
+        indexlist = sp.Server_Peer.get_indexlist()
+        print indexlist
+        for j in indexlist:
+            if j.rfc_num == rfcsearch:
+                print "{} has required RFC {}.".format(j.peer_hostname, rfcsearch)
+                flag = True
+                hostname = j.peer_hostname
 if flag:
     c.getrfc(rfcsearch,hostname)
-'''
